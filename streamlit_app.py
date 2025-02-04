6315cec46379462fa1e04bb9c3c3b11636386204
lemontreeran <ran.xing@gmail.com>
2025-02-04 01:52:42 +0000

    Pending changes exported from your codespace

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,27 @@
 import streamlit as st
+from openai import OpenAI
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import random
+import os
+from streamlit_extras.switch_page_button import switch_page
+from st_pages import add_page_title, get_nav_from_toml
+from streamlit_extras.colored_header import colored_header
+from datetime import datetime, timedelta
+import requests
+
+# Setup page.
+st.set_page_config(layout='wide')
+
+sections = st.sidebar.toggle("Sections", value=True, key="use_sections")
+
+
+pg = st.navigation([
+    st.Page("pages/Home.py", title="Home", icon="🔥"),
+    st.Page("pages/Chatbot.py", title="Chatbot", icon="🤖"),
+    st.Page("pages/Tableau.py", title="Tableau", icon="🖥️"),
+    # st.Page("pages/Docusign.py", title="Docusign", icon="📑"),
+])
+
+add_page_title(pg)
+
+pg.run()
